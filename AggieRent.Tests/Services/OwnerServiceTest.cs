2dfe37739c4980504d07b97c9072f36797b221d3
Myheartdies <yxy20000223@gmail.com>
2024-08-31 23:55:01 -0500

    #25 Create OwnerServiceTest file. Declare test classes

---
+++ AggieRent.Tests/Services/OwnerServiceTest.cs
@@ -0,0 +1,22 @@
+using AggieRent.DataAccess;
+using AggieRent.Models;
+using AggieRent.Services;
+using Moq;
+using Xunit;
+
+namespace AggieRent.Tests.Services
+{
+    public class OwnerService_GetOwnerByIdShould { }
+
+    public class OwnerService_GetOwnersShould { }
+
+    public class OwnerService_CreateOwnerShould { }
+
+    public class OwnerService_DeleteOwnerShould { }
+
+    public class OwnerService_UpdateOwnerShould { }
+
+    public class OwnerService_ResetOwnerPasswordShould { }
+
+    public class OwnerService_ResetOwnerEmailShould { }
+}
