4c9e4102eae5a6957c0fd7cdd619bb253fda10a0
Myheartdies <yxy20000223@gmail.com>
2024-09-10 21:39:30 -0500

    #25 Implement OwnerService_GetOwnerByIdShould

--- AggieRent.Tests/Services/OwnerServiceTest.cs
+++ AggieRent.Tests/Services/OwnerServiceTest.cs
@@ -6,7 +6,38 @@ using Xunit;
 
 namespace AggieRent.Tests.Services
 {
-    public class OwnerService_GetOwnerByIdShould { }
+    public class OwnerService_GetOwnerByIdShould
+    {
+        [Fact]
+        public void GetApplicantById_ValidId_ThenReturnRelatedUser()
+        {
+            var mockOwnerRepository = new Mock<IOwnerRepository>();
+            List<Owner> owners =
+            [
+                new()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Email = "owner@niceestate.com",
+                    HashedPassword = BC.HashPassword("veryStr0ngP@ssw0rd"),
+                    Name = "Nice estate",
+                },
+            ];
+            mockOwnerRepository
+                .Setup(x => x.Get(It.IsAny<string>()))
+                .Returns((string id) => owners.FirstOrDefault(a => a.Id == id));
+            var ownerService = new OwnerService(mockOwnerRepository.Object);
+
+            var returnedOwner = ownerService.GetOwnerById(owners[0].Id);
+            var expectedOwner = new Owner()
+            {
+                Id = owners[0].Id,
+                Email = owners[0].Email,
+                HashedPassword = owners[0].HashedPassword,
+                Name = owners[0].Name,
+            };
+            Assert.Equivalent(expectedOwner, returnedOwner);
+        }
+    }
 
     public class OwnerService_GetOwnersShould { }
 
